559ed3d875d1009936ea4f1979ff1df5ca3bf708
Ayo <ayorexyshittu@gmail.com>
2023-03-14 02:23:04 +0100

    cluster

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -13,10 +13,10 @@ provider "aws" {
 
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
+    # config_path = "~/.kube/config"
   }
 }
 
 provider "kubernetes" {
-  config_path = pathexpand(var.kube_config)
+  # config_path = pathexpand(var.kube_config)
 }
