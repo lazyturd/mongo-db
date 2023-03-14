ccc9671fedff95fe7575f556a30accd306e38b93
Ayo <ayorexyshittu@gmail.com>
2023-03-14 01:42:27 +0100

    p

--- terraform/modules/kube-prometheus/main.tf
+++ terraform/modules/kube-prometheus/main.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "kube-prometheus" {
   name       = "kube-prometheus-stackr"
-  version    = var.kube-version
+  version    = "36.2.0"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
 }
